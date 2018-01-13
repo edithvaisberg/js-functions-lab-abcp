ab3c95172123471a07a52ca7c2118f192ac0cc49
Edith vaisberg <evaisberg94@gmail.com>
2018-01-13 17:19:19 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-// write your code below!
+// write your code below
+
+function happyHolidays(){
+  console.log("happyHolidays");
+}
+
+function string(happyHolidaysTo){
+return (`happyHolidays, ${name}!`);
+}
+
+function string(happyHolidayTo){
+  return (`Happy ${holiday}, ${name}!`);
+}
+
+function string(holidayCountdown){
+return (`It's ${days} days until ${holiday}!`);
+}
